c3792e3a8ef655c94e64e53b5dd670bf9c6a3c6d
unknown <kcaiosouza@gmail.com>
2021-04-30 09:08:31 -0300

    Tentando rodar a radio

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     images: {
-        domains: ['288oficial.com', 'storage.googleapis.com', 'igrejaemcampinagrande.com.br', 'www.google.com'],
+        domains: ['288oficial.com', 'storage.googleapis.com', 'igrejaemcampinagrande.com.br', 'www.google.com', '192.168.1.9:8000'],
     }
 };
