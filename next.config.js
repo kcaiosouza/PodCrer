203eaecf6afbc13227dfa11424374d02ac8151f3
kcaiosouza <kcaiosouza@gmail.com>
2021-09-12 19:07:21 -0300

    Muitas musicas (Com Api)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   images: {
-    domains: ['storage.googleapis.com', '0.tcp.sa.ngrok.io:17521', 'www.igrejaemcampinagrande.com.br', 'igcgcloud.netlify.app', '288oficial.com'],
+    domains: ['storage.googleapis.com', '0.tcp.sa.ngrok.io:19264', 'www.igrejaemcampinagrande.com.br', 'igcgcloud.netlify.app', '288oficial.com'],
   }
 }
