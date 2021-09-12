953b886766d4b8cc42893003f7f04c2e26d33ed6
kcaiosouza <kcaiosouza@gmail.com>
2021-09-12 19:05:41 -0300

    Muitas musicas (Sem Api)

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.API_URL || 'http://0.tcp.sa.ngrok.io:17521/'
+  baseURL: process.env.API_URL || 'http://localhost:3333/'
 })
