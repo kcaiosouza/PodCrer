3c7b3c993e4cd2d06a8cfa0bd6c2c2238f90498f
kcaiosouza <kcaiosouza@gmail.com>
2021-09-28 23:15:28 -0300

    Novo Link Api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.API_URL || 'https://5ee0-177-37-146-48.sa.ngrok.io/'
+  baseURL: process.env.API_URL || 'https://3add-177-37-146-48.sa.ngrok.io/'
 })
