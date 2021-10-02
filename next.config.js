a4fceeed4e7e1155cc7fab0dd5d5fea15b66820c
kcaiosouza <kcaiosouza@gmail.com>
2021-10-01 22:03:19 -0300

    api nova

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   images: {
-    domains: ['storage.googleapis.com', '3add-177-37-146-48.sa.ngrok.io', 'www.igrejaemcampinagrande.com.br', 'igcgcloud.netlify.app', '288oficial.com'],
+    domains: ['storage.googleapis.com', 'e962-177-37-146-48.sa.ngrok.io', 'www.igrejaemcampinagrande.com.br', 'igcgcloud.netlify.app', '288oficial.com'],
   }
 }
