da1d52f3e28d189e491245404f835c19e29490f1
Caio Souza <59893929+kcaiosouza@users.noreply.github.com>
2022-01-02 11:53:05 -0300

    Novo IP

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.API_URL || 'https://74f0-177-37-146-48.sa.ngrok.io/'
+  baseURL: process.env.API_URL || 'https://90c8-177-37-146-48.sa.ngrok.io/'
 })
