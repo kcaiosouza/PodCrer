5f2cc864287fc54ffec88b8d6b0a614c948c9c93
Caio Souza <59893929+kcaiosouza@users.noreply.github.com>
2023-06-13 20:51:21 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.API_URL ||  'https://2de4-2804-29b8-505d-392b-cc4a-cb4a-1b82-f4a6.ngrok.io/'
+  baseURL: process.env.API_URL ||  'https://4e31-2804-29b8-505d-4226-1860-2472-e4c9-2b0f.ngrok-free.app/'
 })
