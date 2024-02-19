e62ed1d2fd8d8ddde04b0c9a91a46969d65370de
Caio Souza <59893929+kcaiosouza@users.noreply.github.com>
2024-02-18 23:19:27 -0300

    Novas Musicas do 288

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.API_URL ||  'https://34b4-2804-29b8-507a-23-ad74-8f9f-48b2-4a41.ngrok-free.app'
+  baseURL: process.env.API_URL ||  'https://8bd1-2804-29b8-5077-9fcd-610f-8a72-4b78-a2c2.ngrok-free.app'
 })
