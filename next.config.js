b88a780ea9778c54e10f2fb599b8fea0f6716ffd
Caio Souza <kcaiosouza@gmail.com>
2024-02-19 23:10:48 -0300

    PWA Configuration next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 // next.config.js
-const withPWA = require('next-pwa');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+})
 
 module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development'
-  },
   images: {
     domains: [
       'storage.googleapis.com',
