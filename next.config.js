f0786349c0afc5f8667532f58fead92007589812
Caio Souza <kcaiosouza@gmail.com>
2024-02-19 23:03:10 -0300

    PWA Configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
   images: {
     domains: ['storage.googleapis.com', '74f0-177-37-146-48.sa.ngrok.io', 'www.igrejaemcampinagrande.com.br', 'igcgcloud.netlify.app', '288oficial.com'],
+  },
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development"
   }
-}
+})
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -34,6 +34,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
+          <link rel="manifest" href="/manifest.json" />
 
           <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
         </Head>
