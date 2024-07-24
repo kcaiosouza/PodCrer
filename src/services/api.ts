595c59a5a842c56f0a87b2ab17adcd01d84c3155
Caio Souza <59893929+kcaiosouza@users.noreply.github.com>
2024-07-24 17:49:06 -0300

    Servidor Local Mikrotik

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.API_URL ||  'https://80d0-2804-29b8-5077-9fcd-20c5-fe12-95c3-452d.ngrok-free.app'
+  baseURL: process.env.API_URL ||  'https://api.igcgmusic.com.br:3333'
 })
