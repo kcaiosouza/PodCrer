219cfc80dc3e0807b14daf7b080e65a1dbde33df
Caio Souza <59893929+kcaiosouza@users.noreply.github.com>
2024-08-04 11:23:00 -0300

    MikroTik HTTPS

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.API_URL ||  'https://api.igcgmusic.com.br:3333'
+  baseURL: process.env.API_URL ||  'https://206.42.29.10:3333'
 })
